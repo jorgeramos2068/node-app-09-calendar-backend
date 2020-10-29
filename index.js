d6c39ee9d8738a1e124b093bc96a5d8f14cf891e
Jorge Ramos <jorgeramos2068@gmail.com>
2020-10-29 09:04:33 -0700

    Install and configure Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundo!!!');
+const express = require('express');
+
+// Create express server
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({
+    ok: true
+  });
+});
+
+// Listen to requests
+app.listen(4000, () => {
+  console.log('Server running in port 4000');
+});
