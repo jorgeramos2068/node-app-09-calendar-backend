af258778b9581c49659960a853d1e4e1762f76ea
Jorge Ramos <jorgeramos2068@gmail.com>
2020-11-02 11:31:52 -0700

    Add functionality to create a new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,22 @@
 const {validationResult} = require('express-validator');
+const User = require('../models/User');
 
-const createUser = (req, res) => {
-  const {name, email, password} = req.body;
-  return res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email
-  });
+const createUser = async (req, res) => {
+  try {
+    //const {name, email, password} = req.body;
+    const user = new User(req.body);
+    await user.save();
+    return res.status(201).json({
+      ok: true,
+      msg: 'register',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'There was an error while saving'
+    });
+  }
 };
 
 const userLogin = (req, res) => {
