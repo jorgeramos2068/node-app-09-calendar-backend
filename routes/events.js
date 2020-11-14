31b1f9eccf684026f17c5af564bbe17fc6976ad8
Jorge Ramos <jose.ramos@nextiva.com>
2020-11-14 10:59:41 -0600

    Remove useless comments

--- routes/events.js
+++ routes/events.js
@@ -33,27 +33,4 @@ router.delete(
   deleteEvent
 );
 
-/*
-// Routes: /api/auth
-router.post(
-  '/new',
-  [
-    check('name', 'Name field is mandatory').not().isEmpty(),
-    check('email', 'Email field is mandatory and must be a valid mail').isEmail(),
-    check('password', 'Password is mandatory and must be 6 characters long').isLength({min: 6}),
-    validateFields
-  ],
-  createUser
-);
-router.post(
-  '/',
-  [
-    check('email', 'Email field is mandatory and must be a valid mail').isEmail(),
-    check('password', 'Password is mandatory and must be 6 characters long').isLength({min: 6}),
-    validateFields
-  ],
-  userLogin
-);
-router.get('/renew', validateJWT, renewToken);*/
-
 module.exports = router;
