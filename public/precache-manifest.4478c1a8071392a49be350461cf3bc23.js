80ab5549b0e0a1098a2f8cf70cf2b66173d68cf5
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:51 -0600

    Update frontend app build to remove Redux Dev Tools

--- public/precache-manifest.4478c1a8071392a49be350461cf3bc23.js
+++ public/precache-manifest.4478c1a8071392a49be350461cf3bc23.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "20e31e4f0f53d731ff6930b803620e84",
+    "revision": "6baf7d38bc37ff5e58b3e54b40f922e7",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ceee16bc.chunk.css"
   },
   {
-    "revision": "fa71b67525debb6e52c4",
+    "revision": "899211102d4cd7b0d223",
     "url": "/static/css/main.29017fc0.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fb868dbe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa71b67525debb6e52c4",
-    "url": "/static/js/main.f40fb9b2.chunk.js"
+    "revision": "899211102d4cd7b0d223",
+    "url": "/static/js/main.cb5a0839.chunk.js"
   },
   {
     "revision": "7e829d7f4f2411c25d99",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c64f09139fbb1231bb084e03a271f04d.js"
+  "/precache-manifest.4478c1a8071392a49be350461cf3bc23.js"
 );
 
 self.addEventListener('message', (event) => {
