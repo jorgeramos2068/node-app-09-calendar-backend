52834a4b01c28071220c4451e896929438df4edf
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:45 -0600

    Add JWT functionality

---
+++ helpers/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const generateJWT = (uid, name) => {
+  return new Promise((resolve, reject) => {
+    const payload = {uid, name};
+    jwt.sign(payload, process.env.JWT_SECRET_SEED, {
+      expiresIn: '2h'
+    }, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject('We could not generate the token');
+      }
+      resolve(token);
+    });
+  });
+};
+
+module.exports = {
+  generateJWT
+}
