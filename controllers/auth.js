1b5fbbab1a596bd5d775254a7095950fe8f74ecf
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:44 -0600

    Add unique email validation when saving a new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,25 @@ const {validationResult} = require('express-validator');
 const User = require('../models/User');
 
 const createUser = async (req, res) => {
+  const {email, password} = req.body;
   try {
-    //const {name, email, password} = req.body;
+    const userToFind = await User.findOne({email: email});
+    if (userToFind) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'That email already exists in the database'
+      });
+    }
     const user = new User(req.body);
     await user.save();
     return res.status(201).json({
       ok: true,
-      msg: 'register',
+      uid: user.id,
+      name: user.name
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: 'There was an error while saving'
     });
