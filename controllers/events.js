3053bf2b4e41033afeb6e24b6cbcd284b578be45
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:49 -0600

    Add functionality to update an event

--- controllers/events.js
+++ controllers/events.js
@@ -35,12 +35,42 @@ const createEvent = async (req, res) => {
   }
 };
 
-const updateEvent = (req, res) => {
+const updateEvent = async (req, res) => {
   const id = req.params.id;
-  return res.status(200).json({
-    ok: true,
-    msg: 'updateEvent',
-  });
+  const uid = req.uid;
+  try {
+    // Verify that the event exists
+    const event = await Event.findById(id);
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'No event was found with that id'
+      });
+    }
+    // Verify that the user is the creator
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have permissions to update this record'
+      });
+    }
+    // Update event
+    const newEvent = {
+      ...req.body,
+      user: uid
+    };
+    const updatedEvent = await Event.findByIdAndUpdate(id, newEvent, {new: true});
+    return res.status(200).json({
+      ok: true,
+      event: updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'An error ocurred while working with the database'
+    });
+  }
 };
 
 const deleteEvent = (req, res) => {
