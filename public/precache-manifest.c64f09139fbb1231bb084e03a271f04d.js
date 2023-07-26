d5399440ac4f7192cfcc5d625c422b5e4fc3c42c
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:51 -0600

    Add frontend application build

---
+++ public/precache-manifest.c64f09139fbb1231bb084e03a271f04d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e31e4f0f53d731ff6930b803620e84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feadbe8eb1807319d74f",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "fa71b67525debb6e52c4",
+    "url": "/static/css/main.29017fc0.chunk.css"
+  },
+  {
+    "revision": "feadbe8eb1807319d74f",
+    "url": "/static/js/2.fb868dbe.chunk.js"
+  },
+  {
+    "revision": "9c53d6eff0828ebc1e689db0e68388c9",
+    "url": "/static/js/2.fb868dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa71b67525debb6e52c4",
+    "url": "/static/js/main.f40fb9b2.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
