6ccefcce68cf7f820e7de918559dc76bbdbd464f
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:47 -0600

    Create routes for events

---
+++ controllers/events.js
@@ -0,0 +1,39 @@
+// const {validationResult} = require('express-validator');
+// const {generateJWT} = require('../helpers/jwt');
+
+const getEvents = (req, res) => {
+  return res.status(200).json({
+    ok: true,
+    msg: 'getEvents',
+  });
+};
+
+const createEvent = (req, res) => {
+  return res.status(200).json({
+    ok: true,
+    msg: 'createEvent',
+  });
+};
+
+const updateEvent = (req, res) => {
+  const id = req.params.id;
+  return res.status(200).json({
+    ok: true,
+    msg: 'updateEvent',
+  });
+};
+
+const deleteEvent = (req, res) => {
+  const id = req.params.id;
+  return res.status(200).json({
+    ok: true,
+    msg: 'deleteEvent',
+  });
+};
+
+module.exports = {
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent
+};
