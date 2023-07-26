d1d14ec603fa1c736d9a17cdad3459f3910e761e
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 20:09:45 -0600

    Encrypt user password when saving

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async (req, res) => {
@@ -12,6 +13,9 @@ const createUser = async (req, res) => {
       });
     }
     const user = new User(req.body);
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
     await user.save();
     return res.status(201).json({
       ok: true,
